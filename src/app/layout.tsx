e421a1aaa16d09ec43a247bd923d9c971e0b0421
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-08-25 10:52:52 -0500

    🆕 Fondo principal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={poppins.className}>
+      <body className={`${poppins.className} bg-bg-100`}>
         {children}
       </body>
     </html>
