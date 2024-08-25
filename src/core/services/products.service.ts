0ea168d21f40be90099b20bb9dedd9f2ea4a21c2
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-08-24 19:15:05 -0500

    🆕 Obtener productos y uno solo por id

---
+++ src/core/services/products.service.ts
@@ -0,0 +1,33 @@
+import { collection, doc, getDoc, getDocs, limit, query, startAfter } from "firebase/firestore"
+import { LIMIT_PRODUCTS, Product } from "@/core"
+import { db } from "@/core"
+
+export const getProducts = async (start: number, limitProducts: number = LIMIT_PRODUCTS) => {
+  const q = query(collection(db, 'products'), limit(limitProducts), startAfter(start))
+  const data = await getDocs(q)
+  const products: Product[] = []
+
+  data.forEach(doc => {
+    products.push(doc.data() as Product)
+  })
+
+  if (products.length === 0) return {
+    hasNext: false,
+    products, 
+    lastProduct: null
+  }
+
+  return {
+    hasNext: data.docs.length === limitProducts,
+    products, 
+    lastProduct: products[products.length - 1]
+  }
+}
+
+export const getProduct = async (id: string) => {
+  const data = await getDoc(doc(db, 'products', id))
+
+  if (!data.exists()) return null
+
+  return data.data() as Product
+}
