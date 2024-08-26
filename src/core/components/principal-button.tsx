00cea7144492624b192108b6e366e0eeaffddb17
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-08-26 10:04:53 -0500

    🛠️ Transicion en los colores

--- src/core/components/principal-button.tsx
+++ src/core/components/principal-button.tsx
@@ -10,7 +10,7 @@ export const PrincipalButton = ({
 }: Props) => {
   return (
     <Link
-      className={`bg-primary-100 lg:hover:bg-blue-600 text-text-100 text-sm lg:text-base py-1.5 px-3.5 rounded-lg lg:transition-opacity ${className}`}
+      className={`bg-primary-100 lg:hover:bg-blue-600 text-text-100 text-sm lg:text-base py-1.5 px-3.5 rounded-lg lg:transition-colors ${className}`}
       {...props}
     >
       {children}
