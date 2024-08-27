2b936f69f4069f40df52d620e43babb75c0256bf
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-08-27 11:15:34 -0500

    🆕Componente con las redes sociales

---
+++ src/core/components/social.tsx
@@ -0,0 +1,47 @@
+import { Facebook, Instagram, Linkedin, Whatsapp } from "@/core"
+import Link from "next/link"
+
+const social = [
+  {
+    name: 'Whatsapp',
+    icon: <Whatsapp className="w-10 h-10" />,
+    href: "https://api.whatsapp.com/send?phone=3114470929&text=Hola%20Kevin%20Gonzalez%20-%20KG%20Soluciones"
+  }, {
+    name: 'Instagram',
+    icon: <Instagram className="w-10 h-10" />,
+    href: "https://www.instagram.com/kg_soluciones/"
+  }, {
+    name: 'Facebook',
+    icon: <Facebook className="w-10 h-10" />,
+    href: "https://www.facebook.com/profile.php?id=100090741046930"
+  }, {
+    name: 'Linkedin',
+    icon: <Linkedin className="w-10 h-10" />,
+    href: "https://www.linkedin.com/in/kevin-jhoel-gonzalez-perez-9a2109280/"
+  }
+]
+
+interface Props {
+  className?: string
+}
+
+const Social = ({
+  className
+}: Props) => {
+  return (
+    <ul className={`flex items-center justify-center gap-3 ${className}`}>
+      {
+        social.map(({ name, icon, href }) => (
+          <li key={name}>
+            <Link
+              href={href}>
+              {icon}
+            </Link>
+          </li>
+        ))
+      }
+    </ul >
+  )
+}
+
+export default Social
