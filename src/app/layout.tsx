0fa126aa8ed3861c35bc72bba169880522289d51
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-08-28 21:02:30 -0500

    🆕 Metadata y logo favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,27 @@
 import type { Metadata } from "next"
 import "./globals.css"
-import { Footer, poppins } from "@/core"
+import { defaultUrl, Footer, poppins } from "@/core"
 
 export const metadata: Metadata = {
+  metadataBase: new URL(defaultUrl),
   title: "KG Soluciones - Servicios y contácto",
   description: "KG Soluciones ofrece servicios integrales de diseño, modelado e impresión 3D; fabricación y reparación de sistemas de alarma para vehículos y hogares; instalación de cámaras de seguridad, portones eléctricos y domótica; gestión de seguridad y salud en el trabajo; desarrollo y mantenimiento de sistemas automatizados; y soporte técnico en electrónica y electricidad automotriz.",
-  keywords: ["diseño 3D", "modelado 3D", "impresión 3D", "ingeniería electromecánica", "fabricación de piezas plásticas", "sistemas de alarma vehicular", "sistemas de alarma hogar", "cámaras de seguridad", "portones eléctricos", "domótica", "seguridad en el trabajo", "automatización", "mantenimiento de sistemas automatizados", "reparación de tarjetas electrónicas", "electricidad automotriz"]
+  keywords: ["diseño 3D", "modelado 3D", "impresión 3D", "ingeniería electromecánica", "fabricación de piezas plásticas", "sistemas de alarma vehicular", "sistemas de alarma hogar", "cámaras de seguridad", "portones eléctricos", "domótica", "seguridad en el trabajo", "automatización", "mantenimiento de sistemas automatizados", "reparación de tarjetas electrónicas", "electricidad automotriz"],
+  openGraph: {
+    title: "KG Soluciones - Servicios y contácto",
+    description: "KG Soluciones ofrece servicios integrales de diseño, modelado e impresión 3D; fabricación y reparación de sistemas de alarma para vehículos y hogares; instalación de cámaras de seguridad, portones eléctricos y domótica; gestión de seguridad y salud en el trabajo; desarrollo y mantenimiento de sistemas automatizados; y soporte técnico en electrónica y electricidad automotriz.",
+    url: defaultUrl,
+    siteName: "KG Soluciones - Servicios y contácto",
+    images: [
+      {
+        url: "/logo.webp",
+        width: 1200,
+        height: 630
+      }
+    ],
+    locale: "es-ES",
+    type: "website"
+  }  
 }
 
 export default function RootLayout({
@@ -15,6 +31,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <head>
+        <link rel="icon" href="/logo.webp" />
+      </head>
       <body className={`${poppins.className} bg-bg-100 antialiased overflow-x-hidden`}>
         {children}
         <Footer />
