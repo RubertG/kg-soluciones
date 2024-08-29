0262f7f1e30b4630c9e44c5215ec0b960f8a200c
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-08-29 12:02:08 -0500

    🆕 Animacion al entrar en la pantalla

--- src/landing/components/service.tsx
+++ src/landing/components/service.tsx
@@ -1,3 +1,7 @@
+"use client"
+
+import { motion } from "framer-motion"
+
 interface Props {
   description: string
   image: React.ReactNode
@@ -10,7 +14,25 @@ export const Service = ({
   className
 }: Props) => {
   return (
-    <div className={`bg-bg-card/30 backdrop-blur-[3px] border border-bg-200 rounded-lg p-3.5 text-sm text-text-200 text-center w-[275px] ${className}`}>
+    <motion.div
+      className={`bg-bg-card/30 backdrop-blur-[3px] border border-bg-200 rounded-lg p-3.5 text-sm text-text-200 text-center w-[275px] ${className}`}
+      initial={{
+        translateY: 40,
+        opacity: 0
+      }}
+      whileInView={{
+        translateY: 0,
+        opacity: 1
+      }}
+      viewport={{
+        once: true,
+        margin: "0px 0px -150px 0px"
+      }}
+      transition={{
+        duration: 0.25,
+        ease: "easeInOut"
+      }}
+    >
       <picture
         className="flex h-[80px] items-center justify-center"
       >
@@ -19,6 +41,6 @@ export const Service = ({
       <p className="mt-2">
         {description}
       </p>
-    </div>
+    </motion.div>
   )
 }
