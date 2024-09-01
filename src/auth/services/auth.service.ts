7895869d1c2d8f1eeeb336905a8ffafcdcca45fb
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-01 11:42:13 -0500

    🛠️ Servicios en forma funcional

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -6,36 +6,35 @@ interface LoginResponse {
   error: string | null
 }
 
-export class AuthService {
-  static login = async (email: string, password: string): Promise<LoginResponse> => {
-    try {
-      const { user } = await signInWithEmailAndPassword(auth, email, password)
-      return {
-        user,
-        error: user ? null : "Error al iniciar sesión"
-      }
-    } catch (error) {
-      console.log(error)
+export const loginService = async (email: string, password: string): Promise<LoginResponse> => {
+  try {
+    const { user } = await signInWithEmailAndPassword(auth, email, password)
 
-      return {
-        user: null,
-        error: "Error al iniciar sesión"
-      }
+    return {
+      user,
+      error: user ? null : "Error al iniciar sesión"
+    }
+  } catch (error) {
+    console.log(error)
+
+    return {
+      user: null,
+      error: "Error al iniciar sesión"
     }
   }
+}
 
-  static singOutSession = async (): Promise<Pick<LoginResponse, "error">> => {
-    try {
-      await signOut(auth)
-      return {
-        error: null
-      }
-    } catch (error) {
-      console.log(error)
-      
-      return {
-        error: "Error al cerrar sesión"
-      }
+export const singOutSessionService = async (): Promise<Pick<LoginResponse, "error">> => {
+  try {
+    await signOut(auth)
+    return {
+      error: null
+    }
+  } catch (error) {
+    console.log(error)
+
+    return {
+      error: "Error al cerrar sesión"
     }
   }
 }
