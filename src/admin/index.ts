a68f6cdd2976a5ba46e863036b6e31eae399fd07
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-02 12:23:19 -0500

    🆕 Pagina principal de administracion (Preliminar)

--- src/admin/index.ts
+++ src/admin/index.ts
@@ -1,3 +1,4 @@
 export * from "./services/products.service"
 export * from "./components/protected-route"
 export * from "./components/nav"
+export * from "./components/header"
--- src/app/administracion/page.tsx
+++ src/app/administracion/page.tsx
@@ -1,7 +1,35 @@
+import { Header } from "@/admin"
+import { Categories, Products } from "@/core"
+import Link from "next/link"
+
 function AdminPage() {
   return (
-    <section className="w-full py-5">
-      <h1 className="text-text-100 text-3xl font-bold text-center md:text-4xl">Administración</h1>
+    <section className="w-full py-7">
+      <Header title="Administración" />
+      <ul className="flex flex-wrap items-start justify-center md:justify-start gap-4">
+        <li className="bg-bg-card/30 rounded-lg p-3.5 border border-bg-200 w-[200px]">
+          <div>
+            <Categories className="w-12 h-12 m-auto" />
+          </div>
+          <h2 className="font-bold text-text-200 text-center text-lg mt-4">Categorías</h2>
+          <Link 
+            href="/administracion/categorias"
+            className="text-text-300 text-center text-sm block lg:hover:text-primary-100 lg:transition-colors"
+            title="Ir a la seccion de categorias"
+          >Más información</Link>
+        </li>
+        <li className="bg-bg-card/30 rounded-lg p-3.5 border border-bg-200 w-[200px]">
+          <div>
+            <Products className="w-12 h-12 m-auto" />
+          </div>
+          <h2 className="font-bold text-text-200 text-center text-lg mt-4">Productos</h2>
+          <Link 
+            href="/administracion/productos"
+            className="text-text-300 text-center text-sm block lg:hover:text-primary-100 lg:transition-colors"
+            title="Ir a la seccion de productos"
+          >Más información</Link>
+        </li>
+      </ul>
     </section>
   )
 }
