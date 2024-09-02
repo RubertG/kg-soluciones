561f2259c4bc90deacd523d9241b45ac4e654173
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-02 17:38:54 -0500

    🛠️ Resetear formulario

--- src/admin/components/category-form.tsx
+++ src/admin/components/category-form.tsx
@@ -1,11 +1,12 @@
 "use client"
 
 import { FormError, Input, PrincipalActionButton, useForm } from "@/core"
-import { categorySchema, saveCategory } from "@/admin"
+import { categorySchema, saveCategory, useCategoryTableStore } from "@/admin"
 import { CategoryInputs } from "../types/category"
 import { Category } from "@/core/types/db/db"
 import { Timestamp } from "firebase/firestore"
 import { toast } from "sonner"
+import { useRef } from "react"
 
 interface Props {
   className?: string
@@ -14,6 +15,8 @@ interface Props {
 export const CategoryForm = ({
   className
 }: Props) => {
+  const addCategory = useCategoryTableStore(state => state.addCategory)
+  const formRef = useRef<HTMLFormElement>(null)
   const { errors, handleSubmit, loading, register } = useForm<CategoryInputs>({
     schema: categorySchema,
     actionSubmit: async (inputs) => {
@@ -25,7 +28,9 @@ export const CategoryForm = ({
       const { success, error } = await saveCategory(newCategory)
 
       if (success) {
+        addCategory(newCategory)
         toast.success(success)
+        formRef.current?.reset()
       } else {
         toast.error(error)
       }
@@ -36,6 +41,7 @@ export const CategoryForm = ({
     <form
       className={`w-full max-w-lg p-3 lg:p-5 border border-bg-200 rounded-lg bg-bg-card/30 ${className}`}
       onSubmit={handleSubmit}
+      ref={formRef}
     >
       <h2 className="text-text-100 text-2xl font-bold text-center">
         Agregar categoría
