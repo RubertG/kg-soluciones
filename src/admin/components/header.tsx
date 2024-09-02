c48ee5ac48ee2340be277d081e52d5281710b732
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-02 12:22:24 -0500

    🆕 Header para las secciones de la administracion

---
+++ src/admin/components/header.tsx
@@ -0,0 +1,17 @@
+interface Props {
+  title: string
+  children?: React.ReactNode
+  className?: string
+}
+
+export const Header = ({
+  children, title, className
+}: Props) => {
+  return (
+    <header className={className}>
+      <h1 className="text-text-100 text-3xl font-bold md:text-4xl">{title}</h1>
+      {children}
+      <hr className="my-4 border-bg-300" />
+    </header>
+  )
+}
