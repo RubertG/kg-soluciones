c7e7e598c887f2075a63fb9205a3f5e195c2fc6c
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-03 12:21:44 -0500

    🛠️ Devolver error en caso que falle el traer los productos

--- src/core/services/products.service.ts
+++ src/core/services/products.service.ts
@@ -4,24 +4,37 @@ import { db } from "@/core"
 import { Product } from "../types/db/db"
 
 export const getProducts = async (start: number, limitProducts: number = LIMIT_PRODUCTS) => {
-  const q = query(collection(db, 'products'), limit(limitProducts), startAfter(start))
-  const data = await getDocs(q)
-  const products: Product[] = []
+  try {
+    const q = query(collection(db, 'products'), limit(limitProducts), startAfter(start))
+    const data = await getDocs(q)
+    const products: Product[] = []
 
-  data.forEach(doc => {
-    products.push(doc.data() as Product)
-  })
+    data.forEach(doc => {
+      products.push(doc.data() as Product)
+    })
 
-  if (products.length === 0) return {
-    hasNext: false,
-    products, 
-    lastProduct: null
-  }
+    if (products.length === 0) return {
+      hasNext: false,
+      products,
+      lastProduct: null,
+      error: null
+    }
 
-  return {
-    hasNext: data.docs.length === limitProducts,
-    products, 
-    lastProduct: products[products.length - 1]
+    return {
+      hasNext: data.docs.length === limitProducts,
+      products,
+      lastProduct: products[products.length - 1],
+      error: null
+    }
+  } catch (error) {
+    console.log(error)
+
+    return {
+      hasNext: false,
+      products: [],
+      lastProduct: null,
+      error: 'Error al obtener productos'
+    }
   }
 }
 
