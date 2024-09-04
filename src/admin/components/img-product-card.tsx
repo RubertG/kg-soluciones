f85ec198d01e44785d6b3e1ec8c32023214a6266
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-04 15:29:50 -0500

    🆕 Card de la imagen del producto en un componente

---
+++ src/admin/components/img-product-card.tsx
@@ -0,0 +1,41 @@
+import { Image as ImageType } from '@/core/types/db/db'
+import Image from 'next/image'
+import React from 'react'
+import { returnFileSize } from '../utils/return-file-size'
+
+interface Props {
+  img: ImageType
+  handleDeleteImage: (img: ImageType) => void
+}
+
+export const ImgProductCard = ({
+  img, handleDeleteImage
+}: Props) => {
+  return (
+    <li
+      className="flex gap-2 items-center lg:hover:bg-bg-200 lg:transition-colors pr-4 rounded-lg"
+    >
+      <Image
+        src={img.url || ""}
+        alt={img.name || ""}
+        width={100}
+        height={100 * 0.33}
+        className="object-cover w-24 aspect-[4/3] rounded-lg"
+      />
+      <div className="w-full">
+        <p className="font-bold text-text-100 flex gap-2 items-center flex-wrap">
+          <span className="whitespace-nowrap">{returnFileSize(img.size)}</span>
+          <span className="text-sm font-normal text-text-300 overflow-hidden text-ellipsis whitespace-nowrap">
+            {img.name}
+          </span>
+        </p>
+        <button
+          className="text-sm text-text-200 lg:hover:text-red-600 lg:transition-colors"
+          onClick={() => handleDeleteImage(img)}
+        >
+          Eliminar
+        </button>
+      </div>
+    </li>
+  )
+}
--- src/admin/components/product-images-form.tsx
+++ src/admin/components/product-images-form.tsx
@@ -1,10 +1,9 @@
 "use client"
 
 import { FileInput } from "@/core"
-import { returnFileSize, useProductImagesFormStore } from "@/admin"
+import { ImgProductCard, returnFileSize, useProductImagesFormStore } from "@/admin"
 import { ChangeEvent } from "react"
 import { Image as ImageType } from "@/core/types/db/db"
-import Image from "next/image"
 
 export const ProductImagesForm = () => {
   const images = useProductImagesFormStore(state => state.images)
@@ -48,32 +47,11 @@ export const ProductImagesForm = () => {
       <ul className="grid grid-cols-1 gap-3 mt-4">
         {
           images.map((img, index) => (
-            <li
+            <ImgProductCard
               key={index}
-              className="flex gap-2 items-center lg:hover:bg-bg-200 lg:transition-colors pr-4 rounded-lg"
-            >
-              <Image
-                src={img.url || ""}
-                alt={img.name || ""}
-                width={100}
-                height={100 * 0.33}
-                className="object-cover w-24 aspect-[4/3] rounded-lg"
-              />
-              <div className="w-full">
-                <p className="font-bold text-text-100 flex gap-2 items-center">
-                  {returnFileSize(img.size)}
-                  <span className="text-sm font-normal text-text-300 overflow-hidden text-ellipsis text-nowrap">
-                    {img.name}
-                  </span>
-                </p>
-                <button
-                  className="text-sm text-text-200 lg:hover:text-red-600 lg:transition-colors"
-                  onClick={() => handleDeleteImage(img)}
-                >
-                  Eliminar
-                </button>
-              </div>
-            </li>
+              img={img}
+              handleDeleteImage={handleDeleteImage}
+            />
           ))
         }
       </ul>
