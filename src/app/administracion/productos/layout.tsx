d48d54b6c41486396c643f2e10d330fcbbe74516
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-04 14:40:25 -0500

    🛠️ Especificacion del limite de tamaño de las imagenes en total

--- src/app/administracion/productos/layout.tsx
+++ src/app/administracion/productos/layout.tsx
@@ -1,4 +1,4 @@
-import { Header } from "@/admin"
+import { Header, LIMIT_SIZE, returnFileSize } from "@/admin"
 
 function ProductsLayout({
   children
@@ -9,7 +9,7 @@ function ProductsLayout({
     <main className="py-7">
       <Header title="Productos">
         <p className="text-text-200 text-sm lg:text-base mt-3">
-          Administra todos los productos de tu catálogo. En esta sección, puedes añadir, editar y eliminar productos de manera eficiente. Asegúrate de proporcionar detalles claros y precisos para cada producto, incluyendo nombre, descripción, imagenes en formato <strong>4/3</strong> y precio, para mantener tu inventario bien organizado y actualizado. Te recomendamos subir las imagenes con extención webp y que cada una pese menos de 200 KB.
+          Administra todos los productos de tu catálogo. En esta sección, puedes añadir, editar y eliminar productos de manera eficiente. Asegúrate de proporcionar detalles claros y precisos para cada producto, incluyendo nombre, descripción, imagenes en formato <strong>4/3</strong> y precio, para mantener tu inventario bien organizado y actualizado. Te recomendamos subir las imagenes con extención webp y que el total de las imagenes sea menos de <strong>{returnFileSize(LIMIT_SIZE)}</strong>.
         </p>
       </Header>
       {children}
