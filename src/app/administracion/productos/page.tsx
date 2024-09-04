05ffa50e3af2e8d010e2ac6c8de50f2a45dae842
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-04 10:45:45 -0500

    🆕 Link para ir a crear nuevo producto

--- src/app/administracion/productos/page.tsx
+++ src/app/administracion/productos/page.tsx
@@ -1,8 +1,15 @@
 import { ProductsTable } from "@/admin"
+import { PrincipalButton } from "@/core"
 
 function ProductsPage() {
   return (
     <section className="flex flex-col flex-wrap gap-5 items-start">
+      <PrincipalButton
+        className="w-full md:w-auto text-center"
+        href="/administracion/productos/nuevo"
+      >
+        Crear nuevo producto
+      </PrincipalButton>
       <ProductsTable />
     </section>
   )
