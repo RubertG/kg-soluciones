e0618fa617b3b1d048b3d21c0e985ea8372a6ed6
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-04 11:12:48 -0500

    🆕 Componente Input select

--- src/core/components/icons.tsx
+++ src/core/components/icons.tsx
@@ -65,3 +65,7 @@ export const X = ({ className }: { className?: string }) => (
     <span className="absolute bg-text-200 lg:group-hover:bg-text-100 lg:transition-colors w-6 h-0.5 rounded-lg -rotate-45" />
   </div>
 )
+
+export const Selector = ({ className }: Props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`icon icon-tabler icons-tabler-outline icon-tabler-selector ${className}`}><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M8 9l4 -4l4 4" /><path d="M16 15l-4 4l-4 -4" /></svg>
+)
--- src/core/components/input.tsx
+++ src/core/components/input.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx'
-import React, { forwardRef } from 'react'
+import React, { DetailedHTMLProps, forwardRef, InputHTMLAttributes, LegacyRef } from 'react'
+import { Selector } from './icons'
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   labelText?: string
@@ -38,3 +39,52 @@ export const Input = forwardRef(function Input({
     </label>
   )
 })
+
+interface SelectInputProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLSelectElement>, HTMLSelectElement> {
+  items?: {
+    name: string
+    id: string
+  }[]
+  title: string,
+  isValid?: boolean
+}
+
+export const SelectInput = forwardRef(
+  function SelectInput({
+    className, isValid, items, title, ...props
+  }: SelectInputProps,
+    ref: LegacyRef<HTMLSelectElement> | undefined
+  ) {
+    return (
+      <div className="relative cursor-pointer">
+        <select
+          className={
+            clsx(`border border-bg-200 bg-bg-card/30 rounded-lg px-3.5 py-2.5 w-full focus:outline-none focus:border-primary-100 transition-colors text-text-200 placeholder:text-text-300 text-sm ${className}`, {
+              "focus:border-primary-100": isValid,
+              "focus:border-red-500": !isValid
+            })
+          }
+          defaultValue={title}
+          {...props} {...(ref == undefined) ? {} : { ref }}>
+          <option
+            className="text-text-300 py-1 bg-bg-card hover:bg-bg-200"
+            disabled
+            selected
+            aria-selected
+            value="">{title}</option>
+          {
+            items?.map((item) => (
+              <option
+                className="text-text-200 py-1 bg-bg-card cursor-pointer hover:bg-bg-200"
+                key={item.id}
+                value={item.id}
+              >
+                {item.name}
+              </option>
+            ))
+          }
+        </select>
+        <Selector className="absolute stroke-text-200 right-3 top-1/2 -translate-y-1/2" />
+      </div>
+    )
+  })
