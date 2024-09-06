5883b5b4955328321cfc4decb790d32cc3770e98
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-06 12:23:14 -0500

    🆕 Icono de lupa para el buscador

--- src/core/components/icons.tsx
+++ src/core/components/icons.tsx
@@ -73,3 +73,7 @@ export const Selector = ({ className }: Props) => (
 export const Upload = ({ className }: Props) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`icon icon-tabler icons-tabler-outline icon-tabler-upload ${className}`}><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" /><path d="M7 9l5 -5l5 5" /><path d="M12 4l0 12" /></svg>
 )
+
+export const SearchIcon = ({ className }: Props) => (
+  <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className={`icon icon-tabler icons-tabler-outline icon-tabler-search ${className}`}><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
+)
