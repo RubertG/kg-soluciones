eb71edb59081d151013edc9bf5e310868452a176
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-06 12:23:04 -0500

    🆕 Buscador

---
+++ src/catalog/components/searcher.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { SearchIcon } from "@/core"
+import { useRouter } from "next/navigation"
+
+interface Props {
+  searchParams: { [key: string]: string | undefined }
+}
+
+export const Searcher = ({
+  searchParams
+}: Props) => {
+  const router = useRouter()
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const name = formData.get('name') as string
+    const newUrl = new URLSearchParams({
+      ...(searchParams.categoria && { categoria: searchParams.categoria }),
+      ...(name && { nombre: name })
+    })
+
+    router.push(`/catalogo?${newUrl.toString()}`)
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="w-full flex justify-center items-center gap-3 border border-bg-200 bg-bg-card/30 rounded-lg transition-colors text-text-100 placeholder:text-text-300 text-sm max-w-xl pr-2.5"
+    >
+      <input
+        className="w-full bg-transparent outline-none px-3.5 py-2.5 rounded-lg"
+        type="text"
+        name="name"
+        placeholder="Nombre del producto que quieres..."
+        autoComplete="off"
+        defaultValue={searchParams.nombre}
+      />
+      <button>
+        <SearchIcon className="stroke-text-200" />
+      </button>
+    </form>
+  )
+}
