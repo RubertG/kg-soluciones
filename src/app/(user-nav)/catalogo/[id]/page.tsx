813993fdf87d762ec9b1e71af329f599fdd2a579
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-08 13:25:56 -0500

    🆕 Pagina de informacion del producto

--- src/app/(user-nav)/catalogo/[id]/page.tsx
+++ src/app/(user-nav)/catalogo/[id]/page.tsx
@@ -1,13 +1,60 @@
+import { ProductInfoContainer } from "@/catalog"
+import { defaultUrl, getProduct } from "@/core"
+import { BackButton } from "@/core"
+import { Metadata } from "next"
+import { notFound } from "next/navigation"
+
 interface Props {
   params: {
     id: string
   }
 }
 
-export default function Page({ params: { id } }: Props) {
+export const generateMetadata = async ({ params: { id } }: Props): Promise<Metadata> => {
+  const products = await getProduct(id)
+
+  return {
+    title: `${products?.name} - KG Soluciones`,
+    description: `${products?.description} - KG Soluciones`,
+    openGraph: {
+      title: `${products?.name} - KG Soluciones`,
+      description: `${products?.description} - KG Soluciones`,
+      url: `${defaultUrl}/catalogo/${id}`,
+      siteName: `${products?.name} - KG Soluciones`,
+      images: [
+        {
+          url: products?.images[0].url ?? "",
+          width: 1200,
+          height: 630
+        }
+      ],
+      locale: "es-ES",
+      type: "website"
+    }
+  }
+}
+
+async function ProductPage({ params: { id } }: Props) {
+  const product = await getProduct(id)
+
+  if (!product) return notFound()
+
   return (
     <div className="mt-20 text-text-200">
-      Producto con el id {id}
+      <section className="flex items-center gap-3 justify-between">
+        <BackButton href="/catalogo" />
+        {
+          product.price && (
+            <p className="text-accent-300 text-xl">
+              ${product.price}
+            </p>
+          )
+        }
+      </section>
+
+      <ProductInfoContainer product={product} className="mt-6" />
     </div>
   )
 }
+
+export default ProductPage
---
+++ src/catalog/components/product-info-container.tsx
@@ -0,0 +1,50 @@
+import { Product } from "@/core/types/db/db"
+import { ButtonsProducts, CounterProduct, ImgsContainer } from "@/catalog"
+import { ButtonShare, defaultUrl } from "@/core"
+
+interface Props {
+  className?: string
+  product: Product
+}
+
+export const ProductInfoContainer = ({
+  product, className
+}: Props) => {
+  return (
+    <section className={`grid grid-cols-1 lg:grid-cols-[45%_auto] gap-4 lg:gap-5 ${className}`}>
+      <ImgsContainer imgs={product.images} />
+      <aside className="overflow-hidden">
+        <header className="flex gap-2 items-center justify-between">
+          <h2 className="text-xl lg:text-2xl font-bold text-text-100">
+            {product.name}
+          </h2>
+          <ButtonShare
+            url={`${defaultUrl}/catalogo/${product.id}`}
+            title={`${product.name} - KG Soluciones`}
+            text={`¡Te invito a que mires el producto "${product.name}" en KG Soluciones!`}
+          />
+        </header>
+
+        <div className="text-text-200 text-sm lg:text-base mt-3" dangerouslySetInnerHTML={{ __html: product.description }} />
+
+        {
+          product.price && (
+            <>
+              <h2 className="text-lg font-bold text-text-100 mt-5">
+                Selecciona la cantidad
+              </h2>
+              <CounterProduct
+                className="mt-3"
+                price={product.price}
+              />
+            </>
+          )
+        }
+
+        <ButtonsProducts
+          className="mt-6"
+        />
+      </aside>
+    </section>
+  )
+}
