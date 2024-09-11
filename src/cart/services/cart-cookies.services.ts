7ce03eb1eb283c14e4db1208cb2d65282f038e49
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-10 21:30:36 -0500

    🆕 Servicio para manejar el carrito (ver si está, añadir y eliminar)

---
+++ src/cart/services/cart-cookies.services.ts
@@ -0,0 +1,32 @@
+import { getCookie, setCookie } from "cookies-next"
+
+const PATH_NAME = "cart"
+
+export const inCart = (id: string) => {
+  const cart = getCookie(PATH_NAME)
+
+  if (!cart) return false
+
+  const newCart = JSON.parse(cart) as Cart
+
+  return newCart[id] ? true : false
+}
+
+export const addCart = (id: string, quantity?: number) => {
+  const cartCookies = getCookie(PATH_NAME) || "{}"
+  const cart = JSON.parse(cartCookies) as Cart
+
+  if (!cart[id]) cart[id] = {
+    ...(quantity ? { quantity } : {})
+  }
+
+  setCookie(PATH_NAME, JSON.stringify(cart))
+}
+
+export const deleteCart = (id: string) => {
+  const cartCookies = getCookie(PATH_NAME) || "{}"
+  const cart = JSON.parse(cartCookies) as Cart
+
+  delete cart[id]
+  setCookie(PATH_NAME, JSON.stringify(cart))
+}
