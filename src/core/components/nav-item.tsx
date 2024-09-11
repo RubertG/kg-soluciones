2d1451c7d01c6866b994a2932f08bfd0b626f6db
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-10 21:52:46 -0500

    🆕 Boton para redirigir al carrito

--- src/core/components/nav-item.tsx
+++ src/core/components/nav-item.tsx
@@ -3,6 +3,7 @@
 import clsx from "clsx"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { Cart } from "./icons"
 
 interface Props {
   item: {
@@ -20,10 +21,28 @@ export const NavItem = ({
   return (
     <Link
       href={href}
-      className={clsx("block py-2 px-3 lg:py-1.5 lg:px-3 lg:rounded-lg w-full border-b lg:border-none border-bg-100 lg:hover:bg-bg-card/50 lg:transition-colors", {
+      className={clsx("block py-2 px-3 lg:py-1.5 lg:px-3 lg:rounded-lg w-full border-b lg:border-none border-bg-100 lg:hover:bg-bg-card/80 lg:transition-colors", {
         "text-primary-100": isActive
       })}>
       {name}
+    </Link>
+  )
+}
+
+export const CartNavItem = () => {
+  const pathname = usePathname()
+  const isActive = pathname === "/carrito"
+
+  return (
+    <Link
+      className="block py-2 px-3 lg:py-1.5 lg:px-3 lg:rounded-lg w-full border-b lg:border-none border-bg-100 lg:hover:bg-bg-card/80 group lg:transition-colors"
+      title="Ir al carrito de compras"
+      href={"/carrito"}
+    >
+      <Cart className={clsx("w-6 h-6 lg:group-hover:stroke-primary-200 lg:transition-colors mx-auto", {
+        "stroke-primary-200": isActive
+      })}
+       />
     </Link>
   )
 }
