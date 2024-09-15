edd4ab04ed17e6d1ebd2cdb20d06f16cf077afdf
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-15 13:26:47 -0500

    🆕 Pagina del pedido de un producto

--- src/app/(user-nav)/(contact-layout)/carrito/[id]/page.tsx
+++ src/app/(user-nav)/(contact-layout)/carrito/[id]/page.tsx
@@ -1,16 +1,30 @@
+import { getCartServerById, ProductsSummary, QuoteForm } from "@/cart"
+import { BackButton } from "@/core"
+import { notFound } from "next/navigation"
+
 interface Props {
   params: {
     id: string
   }
 }
 
-function CartProductPage({
+async function CartProductPage({
   params: { id }
 }: Props) {
+  const product = await getCartServerById(id)
+
+  if (!product) return notFound()
+
   return (
-    <div className="mt-20 text-text-200">
-      Producto del carrito con id <code>{id}</code>
-    </div>
+    <>
+      <BackButton href="/catalogo" className="mt-20" />
+      <section className="mt-7 text-text-200 flex flex-col-reverse gap-4 mx-auto max-w-lg lg:grid lg:grid-cols-2 lg:gap-7 lg:max-w-none lg:items-start">
+        <QuoteForm products={[product]} />
+        <aside>
+          <ProductsSummary products={[product]} />
+        </aside>
+      </section>
+    </>
   )
 }
 
