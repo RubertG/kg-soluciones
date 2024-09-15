f839b23591c76520b19b21785061152f25c15ddd
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-15 12:26:50 -0500

    🆕 Pagina del carrito general

--- src/app/(user-nav)/(contact-layout)/carrito/page.tsx
+++ src/app/(user-nav)/(contact-layout)/carrito/page.tsx
@@ -1,29 +1,32 @@
-import { getCartServer, ProductsSummary } from "@/cart"
-import { PrincipalButton } from "@/core"
+import { getCartServer, ProductsSummary, QuoteForm } from "@/cart"
+import { BackButton, PrincipalButton } from "@/core"
 
 async function CartPage() {
   const products = await getCartServer()
 
   return (
-    <section className="mt-20 text-text-200 flex flex-col-reverse gap-4 mx-auto lg:grid lg:grid-cols-2 lg:gap-6 lg:max-w-none">
-      {
-        products.length > 0 ? (
-          <>
-            <div></div>
-            <aside>
-              <ProductsSummary products={products} />
-            </aside>
-          </>
-        ) : (
-          <section className="flex flex-col items-center justify-center gap-3">
-            <p className="text-text-100">No hay productos en el carrito :(</p>
-            <PrincipalButton href="/catalogo">
-              Seguir cotizando productos
-            </PrincipalButton>
-          </section>
-        )
-      }
-    </section>
+    <>
+      <BackButton href="/catalogo" className="mt-20" />
+      <section className="mt-7 text-text-200 flex flex-col-reverse gap-4 mx-auto max-w-lg lg:grid lg:grid-cols-2 lg:gap-7 lg:max-w-none lg:items-start">
+        {
+          products.length > 0 ? (
+            <>
+              <QuoteForm products={products} />
+              <aside>
+                <ProductsSummary products={products} />
+              </aside>
+            </>
+          ) : (
+            <section className="flex flex-col items-center justify-center gap-3 lg:col-span-full">
+              <p className="text-text-100">No hay productos en el carrito :(</p>
+              <PrincipalButton href="/catalogo">
+                Seguir cotizando productos
+              </PrincipalButton>
+            </section>
+          )
+        }
+      </section>
+    </>
   )
 }
 
