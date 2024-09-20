aa5d11d2067f32a61cf6952a1157e04b9a222341
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-19 21:37:03 -0500

    🛠️ Overflow de la seccion de contacto

--- src/app/(user-nav)/layout.tsx
+++ src/app/(user-nav)/layout.tsx
@@ -9,7 +9,7 @@ async function FirebaseLayout({
   return (
     <>
       <Nav />
-      <main className="grid grid-rows-2 min-h-[100dvh] max-w-6xl mx-auto px-4 xl:px-0 relative">
+      <main className="grid grid-rows-2 min-h-[100dvh] max-w-6xl mx-auto px-4 xl:px-0 relative overflow-hidden">
         <div className="row-span-full">
           {children}
         </div>
